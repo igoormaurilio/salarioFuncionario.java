5414d1365c4a1aae7052dec06a40e8b15a335bc0
Igor Maurilio <igoor.maurilio@gmail.com>
2024-11-27 01:20:20 -0300

    Add files via upload

---
+++ exB.java
@@ -0,0 +1,23 @@
+package aula11;
+
+import java.util.Scanner;
+
+public class exB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scn = new Scanner (System.in);
+	
+		System.out.println("Nome do funcionário: ");
+       String funcionario = scn.next();
+       
+       System.out.println("Seu salário é: ");
+       String salario = scn.next();  
+       
+
+       
+       
+	}
+
+}
